1d0da6626dce9bd5d8c66af5411cb624bc1046fc
Samuel Behrens <sbehrens1414@gmail.com>
2016-11-30 15:58:13 -0700

    Added frame for adding things to lists

--- js/guest.js
+++ js/guest.js
@@ -27,7 +27,7 @@ function loadData() {
             listRef.on('value', function(snapshot) {
                 if (!snapshot.val().deleted) {
                     if (!snapshot.val().claimed) {
-                        $("#listBody").append("<tr id='" + snapshot.val().itemName.replace(/\s/g, '') + "'><td><p class='help-block'>" + snapshot.val().rank + "</p></td><td>" + snapshot.val().itemName + "</td><td><p class='help-block'>" + snapshot.val().description + "</p></td>" + snapshot.val().link + "<td id='" + i + "'><button name='" + i + "' type='button' class='btn btn-primary get-it'>I'll get it</button></td></tr>");
+                        $("#listBody").append("<tr id='" + snapshot.val().itemName.replace(/\s/g, '') + "'><td><p class='help-block'>" + snapshot.val().rank + "</p></td><td><h5>" + snapshot.val().itemName + "</h5></td><td><p class='help-block'>" + snapshot.val().description + "</p></td>" + snapshot.val().link + "<td id='" + i + "'><button name='" + i + "' type='button' class='btn btn-primary get-it'>I'll get it</button></td></tr>");
                     }
                     else {
                         $("#listBody").append("<tr id='" + snapshot.val().itemName.replace(/\s/g, '') + "'><td><p class='help-block'>" + snapshot.val().rank + "</p></td><td>" + snapshot.val().itemName + "</td><td><p class='help-block'>" + snapshot.val().description + "</p></td>" + snapshot.val().link + "<td><p class='text-danger'>" + snapshot.val().claimer + "</p></td></tr>");
